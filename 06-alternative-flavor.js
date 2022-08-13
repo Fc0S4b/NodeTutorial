5eb93c4a7eb150baefcc9d251beb0807a3ab3bc2
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-13 17:47:46 -0400

    alternative syntax

--- 06-alternative-flavor.js
+++ 06-alternative-flavor.js
@@ -1,6 +1,6 @@
-module.exports.items = ['item1', 'item2']
+module.exports.items = ['item1', 'item2'] //se puede definir directamente en vez de escribir const items = ['item1', 'item2']. Estamos definiendo items como propiedad de exports
 const person = {
   name: 'bob',
 }
-
+// definimos singlePerson como propiedad de export también
 module.exports.singlePerson = person
