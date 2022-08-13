e0009529bc37f49e82ffff93746f9d1fe2bed39e
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-13 18:25:24 -0400

    fs async

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,29 +1,32 @@
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile } = require('fs'); // los métodos ya no llevan el nombre Sync
 
-console.log('start')
+console.log('start');
+// necesitamos pasar una callback con fs async, una vez que ejecutamos todo el código, se ejecutará luego la callback.
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
-  const first = result
+  const first = result;
+  // volvemos a definir una callback
   readFile('./content/second.txt', 'utf8', (err, result) => {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    const second = result
+    const second = result;
+    // otra callback, esto ya sería una callback-hell
     writeFile(
       './content/result-async.txt',
       `Here is the result : ${first}, ${second}`,
       (err, result) => {
         if (err) {
-          console.log(err)
-          return
+          console.log(err);
+          return;
         }
-        console.log('done with this task')
+        console.log('done with this task');
       }
-    )
-  })
-})
-console.log('starting next task')
+    );
+  });
+});
+console.log('starting next task');
