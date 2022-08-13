3ba966794ae92f79a865ec01cf63d2c763c9433c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-13 18:17:06 -0400

    fs sync

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,12 +1,12 @@
-const { readFileSync, writeFileSync } = require('fs')
-console.log('start')
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const { readFileSync, writeFileSync } = require('fs'); //destructurando los métodos que necesitamos
+console.log('start');
+const first = readFileSync('./content/first.txt', 'utf8'); //utf8 para decodificar first.txt
+const second = readFileSync('./content/second.txt', 'utf8');
 
 writeFileSync(
-  './content/result-sync.txt',
+  './content/result-sync.txt', //node creará el archivo si no existe
   `Here is the result : ${first}, ${second}`,
-  { flag: 'a' }
-)
-console.log('done with this task')
-console.log('starting the next one')
+  { flag: 'a' } //parámetro que hace que el contenido se abra, mostrando el resultado en un nuevo valor
+);
+console.log('done with this task');
+console.log('starting the next one');
