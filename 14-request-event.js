03f09b1c3fafada9f3f6029d3cc6f1c23a8c0856
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-14 18:10:26 -0400

    events emitter http module example

--- 14-request-event.js
+++ 14-request-event.js
@@ -1,15 +1,17 @@
-const http = require('http')
+const http = require('http');
 
 // const server = http.createServer((req, res) => {
 //   res.end('Welcome')
 // })
 
 // Using Event Emitter API
-const server = http.createServer()
+const server = http.createServer();
 // emits request event
+// responder a :
 // subcribe to it / listen for it / respond to it
+// en vez de usar la callback de createServer, usamos on directamente ya que server es una clase que se extiende a los eventsEmitter
 server.on('request', (req, res) => {
-  res.end('Welcome')
-})
+  res.end('Welcome');
+});
 
-server.listen(5000)
+server.listen(5000);
