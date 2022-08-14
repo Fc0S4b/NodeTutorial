a506e21b41fba6e598cfb6e4a23c0a22523e8f43
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-14 18:02:54 -0400

    events emitter code example

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -1,21 +1,26 @@
+// event-driven programming: programación en base a los eventos como "click", "hover", cuando ser carga la página, etc
 // get back the class
+// extender la clase para personalizar o crear instancia para emitir y manejar eventos
 // if want custom extend from class
 // otherwise just for emitting and handling events create instance
-const EventEmitter = require('events')
+const EventEmitter = require('events'); //es una clase
 
-const customEmitter = new EventEmitter()
+const customEmitter = new EventEmitter(); // creamos una instancia de la clase
 
 // on and emit methods
+// on - listen for an event
+// emit - emit an event
 // keep track of the order
 // additional arguments
 // built-in modules utilize it
 
+// el evento se llama response
 customEmitter.on('response', (name, id) => {
-  console.log(`data recieved user ${name} with id:${id}`)
-})
+  console.log(`data recieved user ${name} with id:${id}`);
+});
 
 customEmitter.on('response', () => {
-  console.log('some other logic here')
-})
-
-customEmitter.emit('response', 'john', 34)
+  console.log('some other logic here');
+});
+// emitimos john 34 y el evento al que va dirigido
+customEmitter.emit('response', 'john', 34);
