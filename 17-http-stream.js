52a99bfd50a4988c6c5c53d166e57f54dc3bc88c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-14 18:33:45 -0400

    streams http example

--- 17-http-stream.js
+++ 17-http-stream.js
@@ -1,16 +1,17 @@
-var http = require('http')
-var fs = require('fs')
+var http = require('http');
+var fs = require('fs');
 
 http
   .createServer(function (req, res) {
     // const text = fs.readFileSync('./content/big.txt', 'utf8')
-    // res.end(text)
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    // res.end(text) //veremos todo el contenido de big.txt en pantalla del navegador pero el tamaño será un problema para enviarse como solicitud a la red
+    // ahora aplicamos stream para manejar por parte esa cantidad de flujo
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
     fileStream.on('open', () => {
-      fileStream.pipe(res)
-    })
+      fileStream.pipe(res); // leer por parte, como una llave que controla el flujo
+    });
     fileStream.on('error', (err) => {
-      res.end(err)
-    })
+      res.end(err);
+    });
   })
-  .listen(5000)
+  .listen(5000);
