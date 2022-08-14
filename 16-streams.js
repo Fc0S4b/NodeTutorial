2631593a0d523c05646e5a91586ff51c785d1a3c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-14 18:13:53 -0400

    streams info

--- 16-streams.js
+++ 16-streams.js
@@ -1,13 +1,20 @@
-const { createReadStream } = require('fs')
+// streams son usados para leer o escribir secuencialmente. Útiles cuando se trata de usar streaming data o grandes archivos
+// tipos de streams
+// writeable: para escribir streams
+// readable: para leer streams
+// duplex: que hace ambas
+// transform: para modificar datos cuando son writeable o readable
+// streams se extienden a la clase de emitters. Son built-in
+const { createReadStream } = require('fs');
 
 // default 64kb
 // last buffer - remainder
 // highWaterMark - control size
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('./content/big.txt');
 
 stream.on('data', (result) => {
-  console.log(result)
-})
-stream.on('error', (err) => console.log(err))
+  console.log(result);
+});
+stream.on('error', (err) => console.log(err));
