deb1a5669affbe7a3f65a74c1c822bd0513b641b
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-13 20:09:17 -0400

    http module setup

--- 12-http.js
+++ 12-http.js
@@ -1,6 +1,7 @@
-const http = require('http')
-
+const http = require('http');
+// createServer recibe req que es el método http y res es lo que mandamos de vuelta
 const server = http.createServer((req, res) => {
+  // res.write('mensaje que se ve en el puerto 5000') //write es otro método
   //   if (req.url === '/') {
   //     res.end('Welcome to our home page')
   //   }
@@ -19,16 +20,16 @@ const server = http.createServer((req, res) => {
   // SWITCH TO IF, ELSE IF, ELSE (BELOW)
   // WE COVER THE CAUSE, LATER IN EXPRESS TUTORIAL
   if (req.url === '/') {
-    res.end('Welcome to our home page')
+    res.end('Welcome to our home page');
   } else if (req.url === '/about') {
-    res.end('Here is our short history')
+    res.end('Here is our short history');
   } else {
     res.end(`
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
     <a href="/">back home</a>
-    `)
+    `);
   }
-})
+});
 
-server.listen(5000)
+server.listen(5000); //se configura el puerto escucha
