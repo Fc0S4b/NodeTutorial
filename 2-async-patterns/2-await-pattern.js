8e568f64462ce7e7fa921eb2928f1fb7ada898da
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-14 17:54:02 -0400

    async patterns - node's native option

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,24 +1,28 @@
-const { readFile, writeFile } = require('fs').promises
-// const util = require('util')
+const { readFile, writeFile } = require('fs').promises; // si usas opción nativa de node entonces no agregas .promises y solo usarías readFile o writeFile, pero si no accedes a este método desde require, desbes configurar la opción nativa y usar readFilePromise o writeFilePromise
+// const util = require('util') //opción nativa de node
 // const readFilePromise = util.promisify(readFile)
 // const writeFilePromise = util.promisify(writeFile)
 
+// refactor to async
 const start = async () => {
+  // si tenemos una función async, manejamos los estados con trycatch
   try {
-    const first = await readFile('./content/first.txt', 'utf8')
-    const second = await readFile('./content/second.txt', 'utf8')
+    // con opción nativa de node sería:
+    // const first = await readFilePromise('./content/first.txt', 'utf8')
+    const first = await readFile('./content/first.txt', 'utf8');
+    const second = await readFile('./content/second.txt', 'utf8');
     await writeFile(
       './content/result-mind-grenade.txt',
       `THIS IS AWESOME : ${first} ${second}`,
-      { flag: 'a' }
-    )
-    console.log(first, second)
+      { flag: 'a' } //a de append
+    );
+    console.log(first, second);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-start()
+start();
 
 // setup promise
 // creamos una función para leer cualquier ruta de archivo:
